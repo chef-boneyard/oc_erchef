3bd89f98d77801190beddd5c43b79f502977a853
Marc Paradise <marc@getchef.com>
2015-02-16 23:54:25 -0500

    some cleanup...

--- dev/cookbooks/piab/metadata.rb
+++ dev/cookbooks/piab/metadata.rb
@@ -1,3 +1,4 @@
+name "piab"
 version "0.0.1"
 description "A cookbook for managing a Platform-In-A-Box"
 
@@ -6,10 +7,3 @@ description "A cookbook for managing a Platform-In-A-Box"
 depends "apt"
 depends "build-essential"
 depends "git"
-depends "yum"
-
-# from the monitoring recipe
-depends "estatsd"
-depends "graphite"
-
-depends "mysql"
