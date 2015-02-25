7120ad38094852f49922d2d721142700045fa505
Mark Anderson <mark@chef.io>
2015-02-25 15:28:40 -0800

    Proposed change to add a read-only-users group to org creation.

--- apps/oc_chef_authz/src/oc_chef_authz_org_creator.erl
+++ apps/oc_chef_authz/src/oc_chef_authz_org_creator.erl
@@ -37,7 +37,7 @@
                      groups, nodes, roles, sandboxes, policies,
                      cookbook_artifacts]).
 
--define(GROUPS, [admins, 'billing-admins', clients, users]).
+-define(GROUPS, [admins, 'billing-admins', clients, users, 'read-only-users']).
 
 -define(ALL_PERMS, [create, read, update, delete, grant]).
 
@@ -88,7 +88,7 @@
 
            %% Admins
            {add_acl,
-            [mk_tl(container, ?CONTAINERS), mk_tl(group, [admins, clients, users]), {organization}],
+            [mk_tl(container, ?CONTAINERS), mk_tl(group, [admins, clients, users, 'read-only-users']), {organization}],
             ?ALL_PERMS, [{group, admins}]},
 
            %% users
@@ -98,6 +98,12 @@
            {add_acl, [{container, clients}], [read, delete], [{group, users}]},
            {add_acl, [mk_tl(container, [groups, containers]), {organization}], [read], [{group, users}]},
            {add_acl, [{container, sandboxes}], [create], [{group, users}]},
+
+           %% read only users
+           {add_acl,
+            [mk_tl(container, [cookbooks, data, nodes, roles, environments, policies, cookbook_artifacts, clients, groups, containers])],
+            [read], [{group, 'read-only-users'}]},
+           {add_acl, [{organization}], [read], [{group, 'read-only-users'}]},
 
            %% clients
            {add_acl, [{container, nodes}], [read, create], [{group, clients}]},
